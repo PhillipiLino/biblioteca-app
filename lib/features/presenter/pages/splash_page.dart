d68bdfa75a14cfae325588cca511b42c6777083e
Phillipi Unger Lino <phillipi.lino@gmail.com>
2023-03-05 18:59:55 -0300

    Adjust splash layout

--- lib/features/presenter/pages/splash_page.dart
+++ lib/features/presenter/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:biblioteca/features/stores/splash_page_store.dart';
 import 'package:clean_architecture_utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,18 +10,29 @@ class SplashPage extends StatefulWidget {
   SplashPageState createState() => SplashPageState();
 }
 
-class SplashPageState extends MainPageState<SplashPage, SplashPageStore> {
-  @override
-  void initState() {
-    super.initState();
-    store.checkUser();
-  }
+class SplashPageState extends MainPageState<SplashPage, SplashPageStore>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(vsync: this);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Lottie.asset(
+          'assets/splash.json',
+          repeat: false,
+          width: MediaQuery.of(context).size.width * 0.7,
+          alignment: Alignment.center,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward().whenComplete(
+                () {
+                  store.checkUser();
+                },
+              );
+          },
+        ),
       ),
     );
   }
